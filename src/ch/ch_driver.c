83c1157df8493c5ba003fd3b5937c7eec1d6637a
Karissa Sanchez <t-kasanchez@microsoft.com>
2022-07-25 21:12:10 +0000

    mark unsupported features as unsupported
    Signed-off-by: Karissa Sanchez <t-kasanchez@microsoft.com>

--- src/ch/ch_driver.c
+++ src/ch/ch_driver.c
@@ -954,16 +954,16 @@ chConnectSupportsFeature(virConnectPtr conn,
                            feature);
             return -1;
         case VIR_DRV_FEATURE_MIGRATION_V3:
-        case VIR_DRV_FEATURE_MIGRATION_P2P:
-        case VIR_DRV_FEATURE_MIGRATE_CHANGE_PROTECTION:
         case VIR_DRV_FEATURE_XML_MIGRATABLE:
-        case VIR_DRV_FEATURE_MIGRATION_OFFLINE:
         case VIR_DRV_FEATURE_MIGRATION_PARAMS:
             VIR_DEBUG("ch: VIR_DRV_FEATURE_MIGRATION_PARAMS return 1");
             return 1;
         case VIR_DRV_FEATURE_MIGRATION_V2:
         case VIR_DRV_FEATURE_MIGRATION_DIRECT:
         case VIR_DRV_FEATURE_MIGRATION_V1:
+        case VIR_DRV_FEATURE_MIGRATION_P2P:
+        case VIR_DRV_FEATURE_MIGRATION_OFFLINE:
+        case VIR_DRV_FEATURE_MIGRATE_CHANGE_PROTECTION:
         default:
             return 0;
     }
